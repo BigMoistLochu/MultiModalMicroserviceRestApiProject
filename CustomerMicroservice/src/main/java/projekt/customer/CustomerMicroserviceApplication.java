492848fe208f2decc20993e9dd73c20928ef61b8
BigMoistLochu <krok.konrad1@gmail.com>
2024-05-23 03:13:42 +0200

    remove jpa/msqlconnector from parent pom(added only for pom which is needed), some refactor code

--- CustomerMicroservice/src/main/java/projekt/customer/CustomerMicroserviceApplication.java
+++ CustomerMicroservice/src/main/java/projekt/customer/CustomerMicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerMicroserviceAplication {
+public class CustomerMicroserviceApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerMicroserviceAplication.class, args);
+        SpringApplication.run(CustomerMicroserviceApplication.class, args);
     }
 }
